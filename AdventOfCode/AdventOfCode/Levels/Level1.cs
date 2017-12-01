551970683fd75ec2b0e94585f6c391376f728754
David Mimnagh <davidmimnagh1@googlemail.com>
2017-12-01 13:14:38 +0000

    Advent of Code 2017
    Starting the Advent of Code.
    This commit is Day 1

---
+++ AdventOfCode/AdventOfCode/Levels/Level1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Levels
+{
+    public class Level1
+    {
+        int total { get; set; }
+        int nextNum { get; set; }
+        int currNum { get; set; }
+
+        string Part1(string input)
+        {
+             for ( int i = 0; i < input.Length; i++ )
+             {
+                 currNum = Convert.ToInt32(input[i].ToString());
+                 if ( ( i + 1 ) <= input.Length - 1 )
+                     nextNum = Convert.ToInt32(input[i + 1].ToString());
+                 else
+                     nextNum = Convert.ToInt32(input[0].ToString());
+
+                 if ( currNum == nextNum )
+                     total += currNum;
+             }
+             return total.ToString();
+        }
+
+        string Part2(string input)
+        {
+            int halfLength = input.Length / 2;
+            for ( int i = 0; i < input.Length; i++ )
+            {
+                currNum = Convert.ToInt32(input[i].ToString());
+                int diff = ( input.Length ) - i;
+                int toAdd = halfLength - diff;
+
+                if ( ( i + halfLength ) <= input.Length - 1 )
+                    nextNum = Convert.ToInt32(input[i + halfLength].ToString());
+                else
+                    nextNum = Convert.ToInt32(input[toAdd].ToString());
+
+                if ( currNum == nextNum )
+                    total += currNum;
+            }
+            return total.ToString();
+        }
+
+        public string MainMethod(string inputString)
+        {
+
+            total = 0;
+            nextNum = -1;
+            return "The answer to this level is: " + Part1(inputString);
+            //return "The answer to this level is: " + Part2(inputString);
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using AdventOfCode.Levels;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+
+        public static int LEVELNUMBER { get; set; }
+        public static Level1 l1;
+        static string GetPuzzleInput()
+        {
+            return File.ReadAllText("..\\..\\Files\\level_"+LEVELNUMBER.ToString() +".txt");
+        }
+
+        static void Main(string[] args)
+        {
+            l1 = new Level1();
+            LEVELNUMBER = 1;
+            switch(LEVELNUMBER)
+            {
+                case 1:
+                    Console.WriteLine(l1.MainMethod(GetPuzzleInput()));
+                    break;
+                case 2:
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
